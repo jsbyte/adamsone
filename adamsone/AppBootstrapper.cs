6ddf9811a9b9acab33f527331025e468f7344d3f
jsbyte <87562981+jsbyte@users.noreply.github.com>
2021-07-19 02:40:10 +0800

    Implement DI
    for Window/Event/ViewModel

--- adamsone/AppBootstrapper.cs
+++ adamsone/AppBootstrapper.cs
@@ -11,9 +11,27 @@ namespace Adamsone
 {
     public class AppBootstrapper : BootstrapperBase
     {
+        private readonly SimpleContainer _container = new SimpleContainer();
         public AppBootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+
+            base.Configure();
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
