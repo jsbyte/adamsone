8d22169f1b5abdc6079ac7c3f015fc9e4bd67b30
jsbyte <87562981+jsbyte@users.noreply.github.com>
2021-07-22 00:33:26 +0800

    Added customize dialog for later use

---
+++ adamsone/ViewModels/UserInputDialogViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Adamsone.Infrastructure;
+using Caliburn.Micro;
+
+namespace Adamsone.ViewModels
+{
+    public class UserInputDialogViewModel : PropertyChangedBase
+    {
+        private readonly ICommand _closeCommand;
+
+        public string MessageText { get; set; }
+
+        public string UserInput { get; set; }
+
+        public bool Cancel { get; set; }
+
+        public string ButtonOkContent { get; set; }
+
+        public string ButtonCancelContent { get; set; }
+
+        public UserInputDialogViewModel(Action<UserInputDialogViewModel> closeHandler, string buttonOkContent = "Ok", string buttonCancelContent = "Cancel")
+        {
+            ButtonOkContent = buttonOkContent;
+            ButtonCancelContent = buttonCancelContent;
+            Cancel = false;
+            _closeCommand = new SimpleCommand { ExecuteDelegate = o => closeHandler(this)};
+        }
+
+        public void ButtonOk()
+        {
+            Cancel = false;
+            _closeCommand.Execute(this);
+        }
+
+        public void ButtonCancel()
+        {
+            Cancel = true;
+            _closeCommand.Execute(this);
+        }
+    }
+}
---
+++ adamsone/Views/UserInputDialog.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace Adamsone.Views
+{
+    /// <summary>
+    /// UserInputDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class UserInputDialog : CustomDialog
+    {
+        public UserInputDialog()
+        {
+            InitializeComponent();
+            MinHeight = 500;
+            MaxHeight = 600;
+            MinWidth = 300;
+
+            Loaded += Dialog_Loaded;
+        }
+
+        private void Dialog_Loaded(object sender, RoutedEventArgs e)
+        {
+            TextBoxInput.Focus();
+        }
+
+        private void TextBoxInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    if (this.DataContext != null) ((dynamic)DataContext).ButtonOk();
+                    break;
+
+                case Key.Escape:
+                    if (this.DataContext != null) ((dynamic)DataContext).ButtonCancel();
+                    break;
+            }
+        }
+    }
+}
