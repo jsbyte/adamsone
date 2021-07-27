b0c8406f3bdb77f489ae0acfba4ffa14f087fb1e
jsbyte <87562981+jsbyte@users.noreply.github.com>
2021-07-28 02:11:29 +0800

    Added save button

--- adamsone/ViewModels/SettingsViewModel.cs
+++ adamsone/ViewModels/SettingsViewModel.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Adamsone.Models;
 using MahApps.Metro.IconPacks;
 
 namespace Adamsone.ViewModels
 {
     public class SettingsViewModel : MenuPageViewModelBase
     {
-        public SettingsViewModel() : base("Settings", new PackIconBoxIcons { Kind = PackIconBoxIconsKind.RegularSlider })
-        {
+        private Config _config;
 
+        public Config Config
+        {
+            get => _config;
+            set
+            {
+                _config = value;
+                NotifyOfPropertyChange(nameof(Config));
+            }
+        }
+
+        public MainViewModel MainViewModel { get; set; }
+
+        public SettingsViewModel(MainViewModel mainViewModel) : base("Settings", new PackIconBoxIcons { Kind = PackIconBoxIconsKind.RegularSlider })
+        {
+            MainViewModel = mainViewModel;
+            Config = mainViewModel.ConfigManager.Config;
+        }
+
+        public void ButtonSave()
+        {
+            MainViewModel.ConfigManager.Save();
         }
     }
 }
