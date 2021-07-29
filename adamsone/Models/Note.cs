f8dd0725ff3b95f54f64c724d976ba156edecbcc
jsbyte <87562981+jsbyte@users.noreply.github.com>
2021-07-30 01:38:24 +0800

    Fix content cannot notify to UI

--- adamsone/Models/Note.cs
+++ adamsone/Models/Note.cs
@@ -4,16 +4,28 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
+using Newtonsoft.Json;
 
 namespace Adamsone.Models
 {
-    public class Note
+    [JsonObject(MemberSerialization.OptOut)]
+    public class Note : PropertyChangedBase
     {
         public Guid Id { get; set; }
 
         public DateTime Created { get; set; }
 
-        public string Content { get; set; }
+        private string _content;
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content = value;
+                NotifyOfPropertyChange(nameof(Content));
+            }
+        }
 
         public Note(string content)
         {
