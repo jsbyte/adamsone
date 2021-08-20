afe2f6bc0fe28f6aca4583d4938b965bb7dccc65
jsbyte <87562981+jsbyte@users.noreply.github.com>
2021-08-21 01:33:57 +0800

    Added encryption for user data

--- adamsone/Infrastructure/ConfigManager.cs
+++ adamsone/Infrastructure/ConfigManager.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using Adamsone.Models;
+using NETCore.Encrypt;
+using NETCore.Encrypt.Internal;
 using Newtonsoft.Json;
 
 namespace Adamsone.Infrastructure
@@ -16,6 +18,7 @@ namespace Adamsone.Infrastructure
 
         public Config Config { get; private set; }
 
+        private readonly string _key = "jVDzlw1wVFvy5CH52GAwBjtYT6Z5PXF9";
         public ConfigManager()
         {
             Path = System.IO.Path.Combine(Environment.CurrentDirectory, "store.json");
@@ -30,13 +33,18 @@ namespace Adamsone.Infrastructure
                 Save();
             }
 
-            Config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(Path));
+            Config = JsonConvert.DeserializeObject<Config>(EncryptProvider.AESDecrypt(File.ReadAllText(Path), _key));
         }
 
         public void Save()
         {
-            File.WriteAllText(Path, JsonConvert.SerializeObject(Config));
+            File.WriteAllText(Path, EncryptProvider.AESEncrypt(JsonConvert.SerializeObject(Config), _key));
             Load();
+        }
+
+        public void ExportAllNote()
+        {
+
         }
     }
 }
